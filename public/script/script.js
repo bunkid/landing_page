17a21e8d3ef05fea9f2530ed76eeb6dcbefbd434
linhprovip2002 <dangquangnhatlinh123@gmail.com>
2023-01-13 09:06:29 +0700

    aaa

--- public/script/script.js
+++ public/script/script.js
@@ -48,4 +48,50 @@ for( i = 0; i <coll.length; i++)
     //     })
     // }
 
+  const array = [
+    "./public/img/gallery1.jpg",
+    "./public/img/gallery2.jpg",
+    "./public/img/gallery3.jpg",
+    "./public/img/gallery4.jpg",
+    "./public/img/gallery5.jpg"
+  ]
+  const content = [
+    "laptop",
+    "Ladies",
+    "clock",
+    "aaa",
+    "bbbb"
+  ]
 
+  var modal = document.getElementById('myModal');
+
+  // Get the images and bind an onclick event on each to insert it inside the modal
+  // use its "alt" text as a caption
+  var images = document.getElementsByClassName("img-thumbnail");
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  for(let i = 0; i < images.length; i++){
+    images[i].onclick = function(){
+      modal.style.display = "block";
+      modalImg.src = array[i];
+      captionText.innerHTML = content[i];
+    }
+  }
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  } 
+
+
+  const menu = document.querySelector(".navbar-links");
+  const menuButton = document.querySelector(".navbar__icons");
+  menuButton.addEventListener('click',()=>
+{
+    menu.classList.toggle("navbar__open");
+    menuButton.classList.toggle("open");
+    
+})
